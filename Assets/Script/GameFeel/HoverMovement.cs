ed3ec35ffd8682538c5862d7f7c8a77386f4cafa
GuillaumeGauduchon <guillaume.gauduchon@hotmail.com>
2023-11-16 09:25:03 +0100

    Update HoverMovement
    Ajout d'un offset aléatoire

--- Assets/Script/GameFeel/HoverMovement.cs
+++ Assets/Script/GameFeel/HoverMovement.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class HoverMovement : MonoBehaviour
 {
+    [Header ("Param")]
+    private float offset;
+    public float offsetMax;
+
+    private float offsetMvt;
+    public float offsetMaxMvt;
+
     [Header ("Rotation")]
     public float duration;
     [Range (0,1)]
@@ -27,6 +34,8 @@ public class HoverMovement : MonoBehaviour
 
     void Awake()
     {
+        offset = Random.Range(-offsetMax, offsetMax);
+        offsetMvt = Random.Range(-offsetMaxMvt, offsetMaxMvt);
         StartCoroutine(Animate());
         StartCoroutine(AnimateMvt());
         
@@ -39,12 +48,12 @@ public class HoverMovement : MonoBehaviour
         while (elapsedTime < duration)
         {
             elapsedTime += Time.deltaTime;
-            float strength = curve.Evaluate(elapsedTime/duration);
+            float strength = curve.Evaluate(Mathf.Repeat(elapsedTime/duration + offset, 1 ) );
             this.gameObject.transform.rotation = new Quaternion(0f,0f, strength * maxAngle, 1f); 
             // this.gameObject.transform.Rotate(0f, 0f, strength * maxAngle, Space.World);
             if(useScale)
             {
-                float scaleStrength = scaleCurve.Evaluate(elapsedTime/duration);
+                float scaleStrength = scaleCurve.Evaluate(Mathf.Repeat(elapsedTime/duration + offset, 1) );
                 float corr = Mathf.Abs(scaleStrength) * scaleCorrection; // Correction to apply to the scale
                 this.gameObject.transform.localScale = new Vector3(corr, corr, corr);
             }
@@ -66,7 +75,7 @@ public class HoverMovement : MonoBehaviour
             if(useMvt)
             {
 
-                float mvtStrength = mvtCurve.Evaluate(elapsedTime/mvtDuration);
+                float mvtStrength = mvtCurve.Evaluate(Mathf.Repeat(elapsedTime/mvtDuration + offsetMvt, 1) );
                 float clampedMvt = Mathf.Clamp(mvtStrength * maxMvt, -maxMvt, maxMvt);  // Clamp the movement
                 this.gameObject.transform.localPosition = new Vector3(0, clampedMvt, 0);
      
