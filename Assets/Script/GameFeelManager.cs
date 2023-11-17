7ff9a73a7a26cd26fea506201d16cdc73185e9bb
Cryp3rone <Manach.Nicolas.mn@gmail.com>
2023-11-17 09:34:47 +0100

    input

--- Assets/Script/GameFeelManager.cs
+++ Assets/Script/GameFeelManager.cs
@@ -22,6 +22,15 @@ public class GameFeelManager : MonoBehaviour
     public UnityEvent<bool> OnToggleFlowerDeathSong = new UnityEvent<bool>();
     public bool allowFlowerDeathSong;
 
+    public UnityEvent<bool> OnToggleBackGround = new UnityEvent<bool>();
+    public bool allowBackGround;
+    public UnityEvent<bool> OnToggleAnim = new UnityEvent<bool>();
+    public bool allowAnim;
+    public UnityEvent<bool> OnToggleDistorsion = new UnityEvent<bool>();
+    public bool allowDistorsion;
+    public UnityEvent<bool> OnToggleRandomObject = new UnityEvent<bool>();
+    public bool allowRandomObject;
+
 
     private void Awake()
     {
@@ -36,6 +45,10 @@ public class GameFeelManager : MonoBehaviour
         allowThemeSong = false;
         allowHitSong = false;
         allowFlowerDeathSong = false;
+        allowBackGround = false;
+        allowAnim = false;  
+        allowDistorsion = false;
+        allowRandomObject = false;
 
         OnToggleParticule.Invoke(false);
         OnToggleSprite.Invoke(false);
@@ -43,6 +56,10 @@ public class GameFeelManager : MonoBehaviour
         OnToggleThemeSong.Invoke(false); 
         OnToggleHitSong.Invoke(false);
         OnToggleFlowerDeathSong.Invoke(false);
+        OnToggleBackGround.Invoke(false);
+        OnToggleAnim.Invoke(false);
+        OnToggleDistorsion.Invoke(false);
+        OnToggleRandomObject.Invoke(false);
     }
 
     public void OnParticuleToggle(InputValue inputValue)
@@ -80,5 +97,25 @@ public class GameFeelManager : MonoBehaviour
         allowFlowerDeathSong = !allowFlowerDeathSong;
         OnToggleFlowerDeathSong.Invoke(allowFlowerDeathSong);
     }
+    public void OnBackGroundToggle(InputValue inputValue)
+    {
+        allowBackGround = !allowBackGround;
+        OnToggleBackGround.Invoke(allowBackGround);
+    }
+    public void OnAnimToggle(InputValue inputValue)
+    {
+        allowAnim = !allowAnim;
+        OnToggleAnim.Invoke(allowAnim);
+    }
+    public void OnDistorsionToggle(InputValue inputValue)
+    {
+        allowDistorsion = !allowDistorsion;
+        OnToggleDistorsion.Invoke(allowDistorsion);
+    }
 
+    public void OnRandomObjectToggle(InputValue inputValue)
+    {
+        allowRandomObject = !allowRandomObject;
+        OnToggleRandomObject.Invoke(allowRandomObject);
+    }
 }
